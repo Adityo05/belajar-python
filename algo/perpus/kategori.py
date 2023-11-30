6506f5ec7f5d67fc73df1df735c9e821aca48e1d
Adityo D.S <152466479+Adityo05@users.noreply.github.com>
2023-11-30 12:07:36 +0700

    Update kategori.py

--- algo/perpus/kategori.py
+++ algo/perpus/kategori.py
@@ -4,7 +4,7 @@ def print_buku_by_kategori():
     ts_instance = Tes()
     kategori_list = ["Fiksi", "Non-Fiksi", "Sains", "Komedi"]
 
-    for kategori in kategori_list:
+    if kategori in kategori_list: #Mengubah for kategori menjadi if kategori
         hasil_filter = ts_instance.filter_buku_by_kategori(kategori)
 
         if hasil_filter:
@@ -13,6 +13,11 @@ def print_buku_by_kategori():
                 print(f"Judul: {judul}, Tersedia: {buku_info['tersedia']}")
         else:
             print(f"Tidak ada buku dengan kategori '{kategori}'.")
+    else: #menambahkan else dengan indentasi yang sejajar dengan if kategori
+        print(f"Maaf kategori buku yang anda masukkan tidak tersedia")
+
+#Menambah input kategori
+inp_kategori = input("Masukkan kategori buku:") 
 
 # Contoh pemanggilan fungsi print_buku_by_kategori
-print_buku_by_kategori()
+print_buku_by_kategori(inp_kategori) #menambah parameter inputan pada saat memanggil fungsi def
